55da9aaa396c821e76b0a3b9cf7cdc3d6322fd75
Mayank <mayanksaini4455@gmail.com>
2022-12-24 18:30:49 +0530

    Add responsiveness in navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, Navigate } from "react-router-dom"
 import "../scss/components/_navbar.scss"
 
 
+
 function Navbar() {
     const { user } = useSelector(state => state.authReducer);
+    const [show, setShow] = useState(true);
     const dispacth = useDispatch();
     const logout = () => {
         localStorage.removeItem("MeduimToken");
         dispacth({ type: "LOGOUT" });
         <Navigate to="/" />
     }
+
+    const change = () => {
+        if (show)
+            setShow(false)
+        else
+            setShow(true)
+        console.log(show)
+    }
     return (
         <nav>
-            <div className='nav-container bg-light'>
+            <div className={show ? 'nav-container bg-light' : "hide_shadow nav-container bg-light"}>
                 <div className='nav_row'>
                     <div className='nav_left'>
                         <Link to="/">
@@ -23,27 +33,56 @@ function Navbar() {
                     </div>
                     <div className='nav_right'>
                         {user ? <>
-                            <Link to="/create">
+                            <Link to="/create" class="">
                                 <span>Create Post</span>
                             </Link>
-                            <Link to="/dashboard/1">
+                            <Link to="/dashboard/1" class="">
                                 <span>{user.name}</span>
                             </Link>
-                            <Link onClick={logout}>
+                            <Link onClick={logout} class="">
                                 <span>Logout</span>
-                            </Link></>
+                            </Link>
+
+                        </>
 
                             :
-                            <><Link to="/login">
+                            <><Link to="/login" class="">
                                 <span>Login</span>
                             </Link>
-                                <Link to="/register">
+                                <Link to="/register" class="">
                                     <span>Register</span>
                                 </Link></>
                         }
-
+                        <div className={show ? 'hamburger' : " hamburger hamburger_cross"} onClick={() => { change() }}>
+                            <div id="ham_first"></div>
+                            <div id="ham_sec"></div>
+                            <div id="ham_thrid"></div>
+                        </div>
                     </div>
                 </div>
+            </div>
+            <div className={show ? "hide" : "dropdown"}>
+                {user ? <>
+                    <Link to="/create" class="">
+                        <span>Create Post</span>
+                    </Link>
+                    <Link to="/dashboard/1" class="">
+                        <span>{user.name}</span>
+                    </Link>
+                    <Link onClick={logout} class="">
+                        <span>Logout</span>
+                    </Link>
+
+                </>
+
+                    :
+                    <><Link to="/login" class="">
+                        <span>Login</span>
+                    </Link>
+                        <Link to="/register" class="">
+                            <span>Register</span>
+                        </Link></>
+                }
             </div>
         </nav>
     )
